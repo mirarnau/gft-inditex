3f54e5b21821ff1a5a1e595bc7452f7b8c8af86c
System Administrator <root@Arnaus-MacBook-Pro.local>
2024-12-29 19:26:17 +0100

    Coverage improvement

---
+++ src/test/java/com/gft_inditex/product_pricing_server/ProductPricingServerApplicationTest.java
@@ -0,0 +1,16 @@
+package com.gft_inditex.product_pricing_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductPricingServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Check that Spring context is generates successfully.
+    }
+
+}
